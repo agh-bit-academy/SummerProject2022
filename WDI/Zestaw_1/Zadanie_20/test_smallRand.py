4a1df98109b5cf61589181fe7b9497d836f08e8a
Rusiek <szymon.rusiecki@protonmail.com>
2022-08-01 21:16:56 +0200

    Quick refactor and setup

--- WDI/Zestaw_1/Zadanie_20/test_smallRand.py
+++ WDI/Zestaw_1/Zadanie_20/test_smallRand.py
@@ -1,6 +1,6 @@
 # Szymon Rusiecki
 import pytest
-from ....randTemplates.RFloat import RFloat
+from ....randTemplates.RFloat import RandFixFloat
 from ....readstdout import checkstdout
 from .prog import f as user_sol
 from .sol import f as corr_sol
@@ -9,13 +9,13 @@ MIN_RANGE = 0
 MAX_RANGE = 100
 TEST_NUM = 100
 SMALL_RAND_TESTS = [
-    ((RFloat.gen_random(MIN_RANGE, MAX_RANGE),
-      RFloat.gen_random(MIN_RANGE, MAX_RANGE)))
+    ((RandFixFloat.gen_random(MIN_RANGE, MAX_RANGE),
+      RandFixFloat.gen_random(MIN_RANGE, MAX_RANGE)))
     for _ in range(TEST_NUM)]
 
 
 @pytest.mark.order(2)
-@pytest.mark.dependency(name="testSmallRand_s1z20", depends=["testBasic_s1z20"], scope="session")
+@pytest.mark.dependency(name="testSmallRand_s1t20", depends=["testBasic_s1t20"], scope="session")
 @pytest.mark.parametrize("data", SMALL_RAND_TESTS)
 def test_small_rand(data):
     assert checkstdout(user_sol, corr_sol, data, float_type=True)
