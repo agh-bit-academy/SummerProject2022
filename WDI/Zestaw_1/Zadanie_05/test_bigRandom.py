1c5255042a493a40431d37f9b35e23ee25f88937
MadMach420 <mach.krzsztf@gmail.com>
2022-08-02 19:40:45 +0200

    Add tests for s1t5

---
+++ WDI/Zestaw_1/Zadanie_05/test_bigRandom.py
@@ -0,0 +1,37 @@
+# Krzysztof Mach
+import pytest
+from ....readstdout import checkstdout
+from .prog import f as user_sol
+from .sol import f as corr_sol
+from random import randint
+
+
+NO_OF_TESTS = 100
+
+MIN_RANGE_MID = 10 ** 8
+MAX_RANGE_MID = 10 ** 10
+midRandomTests = [(randint(MIN_RANGE_MID, MAX_RANGE_MID),) for _ in range(NO_OF_TESTS)]
+
+MIN_RANGE_BIG = 10 ** 8
+MAX_RANGE_BIG = 10 ** 10
+bigRandomTests = [(randint(MIN_RANGE_BIG, MAX_RANGE_BIG),) for _ in range(NO_OF_TESTS)]
+
+
+"""
+Wszystkie argumenty funkcji muszą być iterable, inaczej checkstdout się wysypuje,
+więc dałem je do jednoelementowych krotek
+"""
+
+
+@pytest.mark.order(2)
+@pytest.mark.dependency(name="test_big_random_s1t5", depends=["test_basic_s1t5"], scope="session")
+class Test:
+    @pytest.mark.dependency()
+    @pytest.mark.parametrize("arg", midRandomTests)
+    def test_mid(self, arg):
+        assert checkstdout(user_sol, corr_sol, arg, True)
+
+    @pytest.mark.dependency(depends=["test_mid"], scope="class")
+    @pytest.mark.parametrize("arg", bigRandomTests)
+    def test_big(self, arg):
+        assert checkstdout(user_sol, corr_sol, arg, True)
