02f82ad14bf8fd1594d27b5904003861fbdf8c3e
Maciej <siendobry@gmail.com>
2022-08-02 16:51:53 +0200

    Final changes/flake8

--- WDI/Zestaw_2/Zadanie_07/test_basic.py
+++ WDI/Zestaw_2/Zadanie_07/test_basic.py
@@ -28,7 +28,7 @@ class TestBasic:
     @pytest.mark.parametrize("data", BASIC_TESTS)
     def test_basic(self, data):
         assert checkstdout(user_sol, corr_sol, [data])
-        
+
     @pytest.mark.parametrize("data", BASIC_RANDOM_TESTS)
     def test_basic_random(self, data):
         assert checkstdout(user_sol, corr_sol, [data])
