ef6ae89a1c4da87ec1b1d2bdbc77eccc2929b010
NiEkOKsU <bartek.kozera00@gmail.com>
2022-08-02 14:36:19 +0200

    sol.py poprawka

--- WDI/Zestaw_1/Zadanie_11/sol.py
+++ WDI/Zestaw_1/Zadanie_11/sol.py
@@ -2,30 +2,24 @@
 
 from math import sqrt
 
-ITERATOR = 10**6 + 1
-LOWER_BOUND = 1
-START_FOR_NU2_END_FOR_NUM1 = 0
-NUM_DIVISIBLE_BY = 0
-
 
 def f():
-    for i in range(LOWER_BOUND, ITERATOR):
+    for i in range(1, 10**6 + 1):
         num1 = i
-        num2 = START_FOR_NU2_END_FOR_NUM1
+        num2 = 0
 
-        for j in range(LOWER_BOUND, int(sqrt(num1)) + LOWER_BOUND):
-            if num1 % j == NUM_DIVISIBLE_BY:
-                if j == LOWER_BOUND:
+        for j in range(1, int(sqrt(num1)) + 1):
+            if num1 % j == 0:
+                if j == 1:
                     num2 += j
                 else:
                     num2 += j + (num1/j)
 
-        for j in range(LOWER_BOUND, int(sqrt(num2)) + LOWER_BOUND):
-            if num2 % j == NUM_DIVISIBLE_BY:
-                if j == LOWER_BOUND:
+        for j in range(1, int(sqrt(num2)) + 1):
+            if num2 % j == 0:
+                if j == 1:
                     num1 -= j
                 else:
                     num1 -= j + (num2/j)
-        if num1 == START_FOR_NU2_END_FOR_NUM1 and i < num2:  # Wypisuje liczby w kolejności mniejsza, wieksza
+        if num1 == 0 and i < num2:  # Wypisuje liczby w kolejności mniejsza, wieksza
             print(i, int(num2))
-    return
