4e0fb0f28579738c552092acebd698ba893d491a
MadMach420 <mach.krzsztf@gmail.com>
2022-08-02 12:27:26 +0200

    Changed sol.py

--- WDI/Zestaw_1/Zadanie_02/sol.py
+++ WDI/Zestaw_1/Zadanie_02/sol.py
@@ -10,21 +10,21 @@ def f(increasing=True):
     dopisać False do argumentów. To zmienia wynik.
     """
     year = 2022
-    min_sum = year + 1  # zawsze możliwym rozwiązaniem jest 1 + (year - 1), więc aby je znalazło taka suma na start
-    min_tuple = (-1, -1)
+    minSum = year + 1  # zawsze możliwym rozwiązaniem jest 1 + (year - 1), więc aby je znalazło taka suma na start
+    minTuple = (-1, -1)
     a, b = 1, 1
-    while a + b < min_sum:
-        while a + b < min_sum:
-            fib_a, fib_b = a, b
-            while fib_a + fib_b < year:
-                fib_a, fib_b = fib_b, fib_a + fib_b
-                if fib_a + fib_b == year:
-                    min_sum = a + b
-                    min_tuple = (a, b)
+    while a + b < minSum:
+        while a + b < minSum:
+            fibA, fibB = a, b
+            while fibA + fibB < year:
+                fibA, fibB = fibB, fibA + fibB
+                if fibA + fibB == year:
+                    minSum = a + b
+                    minTuple = (a, b)
             b += 1
         a += 1
         b = a if increasing else 1
-    return min_tuple
+    return minTuple
 
 
 print(f())
