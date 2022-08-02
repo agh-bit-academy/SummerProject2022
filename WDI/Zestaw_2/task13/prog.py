83d2823059c9cf04733593b9962517017836410e
irosikoni <irosikoni@gmail.com>
2022-08-02 16:13:05 +0200

    prog completed

---
+++ WDI/Zestaw_2/task13/prog.py
@@ -0,0 +1,7 @@
+# funkcja powinna odpowiadać na pytanie, czy ostatnia cyfra liczby num
+# jest cyfrą unikatową
+
+def unique_number(num):
+
+    #tu należy wprowadzić swoje rozwiązanie
+    return 0
