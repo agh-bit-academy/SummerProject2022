5b8059f38adf74060a518ae6b050d49aefdcd5ef
Maciej <siendobry@gmail.com>
2022-08-02 17:58:11 +0200

    Little changes

--- WDI/Zestaw_2/Zadanie_07/test_Rand.py
+++ WDI/Zestaw_2/Zadanie_07/test_Rand.py
@@ -1,3 +1,4 @@
+# Maciej Sieniek
 import pytest
 from ....readstdout import checkstdout
 from .prog import f as user_sol
@@ -8,14 +9,14 @@ from random import randint
 MIN_RANGE = 0
 MAX_RANGE = 10 ** 9
 TEST_NUM = 100
-TESTS = [
+TEST_RANDOM = [
     randint(MIN_RANGE, MAX_RANGE)
     for _ in range(TEST_NUM)]
 
 
 @pytest.mark.order(2)
-@pytest.mark.dependency(name="test_Rand_s2ex7", scope="session")
+@pytest.mark.dependency(name="test_Rand_s2ex7", depends=["test_basic_s2ex7"], scope="session")
 class Tests:
-    @pytest.mark.parametrize("data", TESTS)
+    @pytest.mark.parametrize("data", TEST_RANDOM)
     def test_random(self, data):
         assert checkstdout(user_sol, corr_sol, [data])
