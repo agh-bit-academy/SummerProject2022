0f27ef000f940a21f10b655dbc38bf4522dee4d1
socz3qqq <soczawa@student.agh.edu.pl>
2022-08-03 14:51:54 +0200

    Added sol.py, __init.py__

---
+++ WDI/Zestaw_1/Zadanie_10/sol.py
@@ -0,0 +1,38 @@
+# Sebastian Soczawa
+# Żeby testy działały krócej należy zmienić 1000000 na coś mniejszego,
+# albo użyć funkcji f_better, która opiera się na matematycznych właściwościach liczb doskonałych --> (*)
+# Hint: Są 4 liczby doskonałe mniejsze od miliona, wszystkie mniejsze od 10 000
+
+def f():
+    for i in range(2, 1000000):
+        sum = 1
+        for div in range(2, int(i**0.5)+1):
+            if i % div == 0:
+                sum += div + i/div
+        if sum == i:
+            print(i)
+
+# Odkomentuj to i zakomentuj poprzednią funkcję
+# -------------------(*)----------------------
+# def prime(n):
+#     if n<2:
+#         return False
+#     if n==2 or n==3:
+#         return True
+#     if n%2 == 0 or n%3==0:
+#         return False
+#     k=6
+#     while k+1<int(n**0.5)+1:
+#         if n%(k-1) == 0 or n%(k+1)==0:
+#             return False
+#         k+=6
+#     return True
+
+# def f(n = 4):
+#     pow2 = 2
+#     while n>0:
+#         pow2 *= 2
+#         if prime(pow2-1):
+#             print(int((pow2-1)*pow2/2))
+#             n-=1
+# -------------------------------------------
