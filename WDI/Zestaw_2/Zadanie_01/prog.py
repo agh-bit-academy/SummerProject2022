b20668c63645dd6300437ca2cb27c486ef958ccb
NiEkOKsU <bartek.kozera00@gmail.com>
2022-08-03 11:56:28 +0200

    prog.py added

---
+++ WDI/Zestaw_2/Zadanie_01/prog.py
@@ -0,0 +1,19 @@
+# Bartłomiej Kozera
+
+
+def f(x):
+    low, high = 1, 1
+
+    while low <= x:
+        tmpLow = low
+        tmpHigh = high
+
+        while tmpHigh <= x:
+            if x % tmpHigh == 0:
+                if x / tmpHigh == low:
+                    return True
+
+            tmpLow, tmpHigh = tmpHigh, tmpLow + tmpHigh
+        low, high = high, low + high
+
+    return False
