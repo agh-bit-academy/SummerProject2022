2ec0772f31d3ae32ca472c3b0d30d7186b61aefd
NiEkOKsU <bartek.kozera00@gmail.com>
2022-08-03 15:42:08 +0200

    usunięty self w argumentach poza klasą

--- WDI/Zestaw_1/Zadanie_08/test_bigRand.py
+++ WDI/Zestaw_1/Zadanie_08/test_bigRand.py
@@ -14,5 +14,5 @@ SMALL_RANDOM_TESTS = [(randint(MIN_RANGE, MAX_RANGE),) for _ in range(TEST_NUM)]
 @pytest.mark.order(3)
 @pytest.mark.dependency(name="test_big_rand_s1t8", depends=["test_small_rand_s1t8"], scope="session")
 @pytest.mark.parametrize("data", SMALL_RANDOM_TESTS)
-def test_big_random(self, data):
+def test_big_random(data):
     assert checkstdout(user_sol, corr_sol, data, float_type=False)
