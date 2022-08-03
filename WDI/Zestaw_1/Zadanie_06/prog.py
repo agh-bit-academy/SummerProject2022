2fcf172ca7fbff36b56c9958adc1a6702945fe36
Szczepan Rzeszutek <szcz113@gmail.com>
2022-08-03 12:34:28 +0200

    add task 6

---
+++ WDI/Zestaw_1/Zadanie_06/prog.py
@@ -0,0 +1,4 @@
+PRECISION=0.000000001
+def f():
+    # Tutaj wprowadź swój kod
+    pass
---
+++ WDI/Zestaw_1/Zadanie_06/sol.py
@@ -0,0 +1,19 @@
+# Szczepan Rzeszutek
+PRECISION = 0.000000001
+
+
+def f():
+    def function(x):
+        return x ** x - 2020
+
+    a, b = 0, 100
+    while abs(a - b) > PRECISION:
+        x = (a + b) / 2
+        if function(x) == 0:
+            print(x)
+        if function(a) * function(x) < 0:
+            b = x
+        elif function(x) * function(b) < 0:
+            a = x
+
+    print((a + b) / 2)
