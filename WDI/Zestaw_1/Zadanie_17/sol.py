49e51767ed74893b246c85e1a28f00db9bbd86a7
Andrzej Karcinski <andrzej.karcinski@gmail.com>
2022-08-03 13:36:31 +0200

    s1t17_sol

--- WDI/Zestaw_1/Zadanie_17/sol.py
+++ WDI/Zestaw_1/Zadanie_17/sol.py
@@ -7,9 +7,9 @@ def f(a=1, b=1):
     bounder = 1
     c = a + b
     while bounder > EPSILON:
-        quotient_before = c / b
+        quotientBefore = c / b
         a, b = b, a + b
         c = a + b
-        quotient_after = c / b
-        bounder = abs(quotient_before - quotient_after)
-    print(quotient_after)
+        quotientAfter = c / b
+        bounder = abs(quotientBefore - quotientAfter)
+    print(quotientAfter)
