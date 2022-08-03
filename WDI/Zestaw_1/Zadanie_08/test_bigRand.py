539cec210b9c673b32d6e401c5b8bbe37751a6ea
NiEkOKsU <bartek.kozera00@gmail.com>
2022-08-03 15:46:04 +0200

    Zmiana arg

--- WDI/Zestaw_1/Zadanie_08/test_bigRand.py
+++ WDI/Zestaw_1/Zadanie_08/test_bigRand.py
@@ -8,11 +8,11 @@ from random import randint
 MIN_RANGE = 10**4
 MAX_RANGE = 10**6
 TEST_NUM = 30
-SMALL_RANDOM_TESTS = [(randint(MIN_RANGE, MAX_RANGE),) for _ in range(TEST_NUM)]
+BIG_RANDOM_TESTS = [(randint(MIN_RANGE, MAX_RANGE),) for _ in range(TEST_NUM)]
 
 
 @pytest.mark.order(3)
 @pytest.mark.dependency(name="test_big_rand_s1t8", depends=["test_small_rand_s1t8"], scope="session")
-@pytest.mark.parametrize("data", SMALL_RANDOM_TESTS)
+@pytest.mark.parametrize("data", BIG_RANDOM_TESTS)
 def test_big_random(data):
     assert checkstdout(user_sol, corr_sol, data, float_type=False)
