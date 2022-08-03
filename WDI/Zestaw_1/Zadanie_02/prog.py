9f0eda584a0d37ecbd0ec19a70d036eb93801ec3
Kasl0 <slonieckacper@gmail.com>
2022-08-03 17:56:44 +0200

    A_s1t2_prog fixed

--- WDI/Zestaw_1/Zadanie_02/prog.py
+++ WDI/Zestaw_1/Zadanie_02/prog.py
@@ -6,10 +6,12 @@ def f():
         for a in range (1, sum):
             a_start = a
             b_start = sum-a
-            a = a_start
-            b = b_start
-            while a <= YEAR:
-                if a == YEAR:
+            if a_start > b_start:
+                continue
+            x = a_start
+            y = b_start
+            while x <= YEAR:
+                if x == YEAR:
                     print(a_start, b_start)
-                    exit()
-                a, b = a + b, a
+                    return
+                x, y = y, x + y
