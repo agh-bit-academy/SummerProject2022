1e419b5b809feb90eb89a938c81a5b0809e3799e
irosikoni <irosikoni@gmail.com>
2022-08-03 20:21:43 +0200

    tried sth with test

--- WDI/Zestaw_1/Zadanie_19/test.py
+++ WDI/Zestaw_1/Zadanie_19/test.py
@@ -1,9 +1,10 @@
-import pytest
+# Izabella Rosikoń
 from ....readstdout import checkstdout
 from .prog import e as user_sol
 from .sol import e as corr_sol
 from random import randint
 
-class TestE:
-    @pytest.mark.parametrize("data")
-    
+SET_PRECISION = 0.1 ** 6
+
+def test_e():
+    assert abs(user_sol() - corr_sol()) < SET_PRECISION
