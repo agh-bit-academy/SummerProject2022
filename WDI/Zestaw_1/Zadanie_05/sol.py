9129a95e3c40ac2418f71ce335673364e6c20f76
Krzysztof Mach <99477121+MadMach420@users.noreply.github.com>
2022-08-03 16:19:21 +0200

    Update sol.py

--- WDI/Zestaw_1/Zadanie_05/sol.py
+++ WDI/Zestaw_1/Zadanie_05/sol.py
@@ -1,9 +1,8 @@
 # Krzysztof Mach
-APPROX_ERROR = 10 ** (-6)
 
 
 def f(num):
     a, b = 1, num
-    while abs(a - b) > APPROX_ERROR:
+    while abs(a - b) > 10 ** (-6):
         a, b = (a + b) / 2, num / ((a + b) / 2)
     print((a + b) / 2)
