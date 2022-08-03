eabf592d6eb84b950a2c03d1263017775816d5d8
socz3qqq <soczawa@student.agh.edu.pl>
2022-08-03 15:32:45 +0200

    Improved comments

--- WDI/Zestaw_1/Zadanie_10/sol.py
+++ WDI/Zestaw_1/Zadanie_10/sol.py
@@ -1,8 +1,4 @@
 # Sebastian Soczawa
-# Żeby testy działały krócej należy zmienić 1000000 na coś mniejszego,
-# albo użyć funkcji f_better, która opiera się na matematycznych właściwościach liczb doskonałych --> (*)
-# Hint: Są 4 liczby doskonałe mniejsze od miliona, wszystkie mniejsze od 10 000
-
 def f():
     for i in range(2, 1000000):
         sum = 1
@@ -12,27 +8,31 @@ def f():
         if sum == i:
             print(i)
 
+# Żeby testy działały krócej należy zmienić 1000000 na coś mniejszego,
+# albo użyć funkcji f poniżej, która opiera się na matematycznych właściwościach liczb doskonałych
+# Hint: Są 4 liczby doskonałe mniejsze od miliona, wszystkie mniejsze od 10 000
 # Odkomentuj to i zakomentuj poprzednią funkcję
 # -------------------(*)----------------------
 # def prime(n):
-#     if n<2:
+#     if n < 2:
 #         return False
-#     if n==2 or n==3:
+#     if n == 2 or n == 3:
 #         return True
-#     if n%2 == 0 or n%3==0:
+#     if n % 2 == 0 or n % 3 == 0:
 #         return False
-#     k=6
-#     while k+1<int(n**0.5)+1:
-#         if n%(k-1) == 0 or n%(k+1)==0:
+#     k = 6
+#     while k + 1 < int(n**0.5) + 1:
+#         if n % (k - 1) == 0 or n % (k + 1) == 0:
 #             return False
-#         k+=6
+#         k += 6
 #     return True
 
-# def f(n = 4):
+
+# def f(n=4):
 #     pow2 = 2
-#     while n>0:
+#     while n > 0:
 #         pow2 *= 2
-#         if prime(pow2-1):
-#             print(int((pow2-1)*pow2/2))
-#             n-=1
+#         if prime(pow2 - 1):
+#             print(int((pow2 - 1) * pow2 / 2))
+#             n -= 1
 # -------------------------------------------
