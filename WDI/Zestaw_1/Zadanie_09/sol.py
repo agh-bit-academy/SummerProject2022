d4280db9982c66cb47c6e645985bba4e46c9b979
puszekjuliuszek <tojabonibykto@gmail.com>
2022-08-04 13:58:45 +0200

    I've made the sollution easier

--- WDI/Zestaw_1/Zadanie_09/sol.py
+++ WDI/Zestaw_1/Zadanie_09/sol.py
@@ -1,21 +1,6 @@
 # Juliusz Wasieleski
-from math import sqrt
-
 
 def f(n):
-    if n == 2 or n == 3:
-        print(1)
-        print(n)
-    elif n != 0:
-        stack = []
-        for i in range(1, int(sqrt(n))):
-            if n % i == 0:
-                print(i)
-                stack.append(n // i)
-
-        if n // int(sqrt(n)) == sqrt(n):
-            print(int(sqrt(n)))
-
-        while len(stack) > 0:
-            i = stack.pop()
+    for i in range(1, n + 1):
+        if n % i == 0:
             print(i)
