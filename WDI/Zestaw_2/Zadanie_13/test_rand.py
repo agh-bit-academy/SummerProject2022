16215b90083d49431c74f268d9db8bf2b2f788d4
irosikoni <irosikoni@gmail.com>
2022-08-05 00:00:03 +0200

    tests finished

--- WDI/Zestaw_2/Zadanie_13/test_rand.py
+++ WDI/Zestaw_2/Zadanie_13/test_rand.py
@@ -6,4 +6,13 @@ from random import randint
 MIN_RANGE = 100
 MAX_RANGE = 1000
 TEST_NUM = 100
-BASIC_RANDOM_TESTS = []
+BASIC_RANDOM_TESTS = [
+    randint(MIN_RANGE, MAX_RANGE)
+    for _ in range(TEST_NUM)
+]
+
+@pytest.mark.order(2)
+@pytest.mark.dependency(name="test_rand_s2t13", depends=["test_basic_s2t13"], scope="session")
+@pytest.mark.parametrize("data", BASIC_RANDOM_TESTS)
+def test_random(data):
+    assert user_sol(data) == corr_sol(data)
