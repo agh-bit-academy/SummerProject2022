f5e03a70d21e48a1acc6b432ad842003f36d26c0
irosikoni <irosikoni@gmail.com>
2022-08-04 15:20:17 +0200

    name changed

--- WDI/Zestaw_1/Zadanie_19/test.py
+++
@@ -1,8 +0,0 @@
-# Izabella Rosikoń
-from .prog import e_number as user_sol
-from .sol import e_number as corr_sol
-
-SET_PRECISION = 0.1 ** 6
-
-def test_e():
-    assert abs(user_sol() - corr_sol()) < SET_PRECISION
