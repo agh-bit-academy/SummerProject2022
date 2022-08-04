fbe95b1f89895d17720f6d2a1a5f0c91729fcb09
Andrzej Karcinski <andrzej.karcinski@gmail.com>
2022-08-04 13:36:04 +0200

    E_s1t07 solution

---
+++ WDI/Zestaw_1/Zadanie_07/sol.py
@@ -0,0 +1,13 @@
+# Andrzej Karciński
+
+
+def f(number):
+    a = 1
+    b = 1
+    c = 2
+    while (a * b) <= number:
+        if a * b == number:
+            return True
+        a, b = b, c
+        c = a + b
+    return False
