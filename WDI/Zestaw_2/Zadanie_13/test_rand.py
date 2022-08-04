fffda26fe279bf067496a313dc51e7fb2fb94489
irosikoni <irosikoni@gmail.com>
2022-08-04 23:42:40 +0200

    new tests, folder name changed

---
+++ WDI/Zestaw_2/Zadanie_13/test_rand.py
@@ -0,0 +1,9 @@
+import pytest
+from .prog import unique_digit as user_sol
+from .sol import unique_digit as corr_sol
+from random import randint
+
+MIN_RANGE = 100
+MAX_RANGE = 1000
+TEST_NUM = 100
+BASIC_RANDOM_TESTS = []
