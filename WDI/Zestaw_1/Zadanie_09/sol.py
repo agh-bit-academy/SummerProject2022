b14d7a1bf19db71a3f599fe4b457690bead905fe
puszekjuliuszek <tojabonibykto@gmail.com>
2022-08-04 13:47:11 +0200

    I've improved sol file

--- WDI/Zestaw_1/Zadanie_09/sol.py
+++ WDI/Zestaw_1/Zadanie_09/sol.py
@@ -1,5 +1,19 @@
 # Juliusz Wasieleski
+
 def f(n):
-    for i in range(1,n+1):
-        if n%i == 0:
+    if n ==2 or n == 3:
+        print(1)
+        print(n)
+    elif n!=0:
+        stack = []
+        for i in range(1, int(sqrt(n))):
+            if n % i == 0:
+                print(i)
+                stack.append(n // i)
+
+        if n // int(sqrt(n)) == sqrt(n):
+            print(int(sqrt(n)))
+
+        while len(stack) > 0:
+            i = stack.pop()
             print(i)
