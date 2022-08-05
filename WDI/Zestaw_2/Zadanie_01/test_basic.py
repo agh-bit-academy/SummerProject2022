3dc25d1f39d765ccc17d6d565f13fff465480647
Kasl0 <slonieckacper@gmail.com>
2022-08-05 18:16:43 +0200

    Poprawiono testy

--- WDI/Zestaw_2/Zadanie_01/test_basic.py
+++ WDI/Zestaw_2/Zadanie_01/test_basic.py
@@ -1,27 +1,26 @@
 # Kacper Słoniec
 import pytest
-from ....readstdout import checkstdout
 from .prog import f as user_sol
 from .sol import f as corr_sol
 from random import randint
 
 BASIC_TESTS = [
-    (1, ),
-    (2, ),
-    (3, ),
-    (4, ),
-    (6, ),
-    (9, ),
-    (17, ),
-    (34, ),
-    (99, ),
-    (100, )]
+    1,
+    2,
+    3,
+    4,
+    6,
+    9,
+    17,
+    34,
+    99,
+    100]
 
 MIN_RANGE = 1
 MAX_RANGE = 100
 TEST_NUM = 90
 BASIC_RANDOM_TESTS = [
-    (randint(MIN_RANGE, MAX_RANGE), )
+    randint(MIN_RANGE, MAX_RANGE)
     for _ in range(TEST_NUM)]
 
 
@@ -30,8 +29,8 @@ BASIC_RANDOM_TESTS = [
 class TestBasic:
     @pytest.mark.parametrize("data", BASIC_TESTS)
     def test_basic(self, data):
-        assert checkstdout(user_sol, corr_sol, data)
+        assert user_sol(data) == corr_sol(data)
 
     @pytest.mark.parametrize("data", BASIC_RANDOM_TESTS)
     def test_basic_random(self, data):
-        assert checkstdout(user_sol, corr_sol, data)
+        assert user_sol(data) == corr_sol(data)
