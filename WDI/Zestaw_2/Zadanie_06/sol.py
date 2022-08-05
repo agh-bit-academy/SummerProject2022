bd2e1498070c3b508c4c79600241e4f609a03e7b
Kasl0 <slonieckacper@gmail.com>
2022-08-05 18:43:32 +0200

    Poprawiono sol.py

--- WDI/Zestaw_2/Zadanie_06/sol.py
+++ WDI/Zestaw_2/Zadanie_06/sol.py
@@ -3,6 +3,6 @@ from math import sqrt
 
 
 def f(n):
-    for factor in range(int(sqrt(n)), n):
+    for factor in range(int(sqrt(n)), 0, -1):
         if n % factor == 0:
             return factor, n//factor
