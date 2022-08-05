5ce30f8ce3afa0a148037bcf3b750ec0d8045a11
socz3qqq <soczawa@student.agh.edu.pl>
2022-08-05 15:24:58 +0200

    prog.py, test_big_random change

--- WDI/Zestaw_2/Zadanie_02/prog.py
+++ WDI/Zestaw_2/Zadanie_02/prog.py
@@ -1,10 +1,15 @@
-def f(a,b,n):
-    #tutaj wpisz swoje rozwiązanie
-    print(a // b, "." if a % b != 0 else "", sep="", end="")
-    a %= b
-    while a > 0 and n > 0:
-        a *= 10
-        print(a // b, end="")
+# Sebastian Soczawa
+def f(a, b, n):
+    # tutaj wpisz swoje rozwiązanie
+    if a <= 0 or b <= 0 or n <= 0:
+        print("Błędne dane")
+    if a % b == 0:
+        print(a // b)
+        return
+    print(a // b,  ".", sep="", end="")
+    for _ in range(n):
         a %= b
-        n -= 1
-    pass
+        if a == 0:
+            return
+        a *= 10
+        print(a // b, sep="", end="")
