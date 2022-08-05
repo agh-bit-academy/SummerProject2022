6cd8ec4e9785949715dd6f6133dfad4d0bf778e4
Kasl0 <slonieckacper@gmail.com>
2022-08-05 18:44:04 +0200

    Poprawiono testy

--- WDI/Zestaw_2/Zadanie_06/test_bigRand.py
+++ WDI/Zestaw_2/Zadanie_06/test_bigRand.py
@@ -1,6 +1,5 @@
 # Kacper Słoniec
 import pytest
-from ....readstdout import checkstdout
 from .prog import f as user_sol
 from .sol import f as corr_sol
 from random import randint
@@ -9,7 +8,7 @@ MIN_RANGE = 1
 MAX_RANGE = 10 ** 6
 TEST_NUM = 100
 BIG_RAND_TESTS = [
-    (randint(MIN_RANGE, MAX_RANGE), )
+    randint(MIN_RANGE, MAX_RANGE)
     for _ in range(TEST_NUM)]
 
 
@@ -18,4 +17,4 @@ BIG_RAND_TESTS = [
 class TestBig:
     @pytest.mark.parametrize("data", BIG_RAND_TESTS)
     def test_big_rand(self, data):
-        assert checkstdout(user_sol, corr_sol, data, float_type=False)
+        assert user_sol(data) == corr_sol(data)
--- WDI/Zestaw_2/Zadanie_06/test_smallRand.py
+++ WDI/Zestaw_2/Zadanie_06/test_smallRand.py
@@ -1,6 +1,5 @@
 # Kacper Słoniec
 import pytest
-from ....readstdout import checkstdout
 from .prog import f as user_sol
 from .sol import f as corr_sol
 from random import randint
@@ -9,7 +8,7 @@ MIN_RANGE = 1
 MAX_RANGE = 1000
 TEST_NUM = 100
 SMALL_RAND_TESTS = [
-    (randint(MIN_RANGE, MAX_RANGE), )
+    randint(MIN_RANGE, MAX_RANGE)
     for _ in range(TEST_NUM)]
 
 
@@ -17,4 +16,4 @@ SMALL_RAND_TESTS = [
 @pytest.mark.dependency(name="test_small_rand_s2t6", depends=["test_basic_s2t6"], scope="session")
 @pytest.mark.parametrize("data", SMALL_RAND_TESTS)
 def test_small_rand(data):
-    assert checkstdout(user_sol, corr_sol, data, float_type=False)
+    assert user_sol(data) == corr_sol(data)
