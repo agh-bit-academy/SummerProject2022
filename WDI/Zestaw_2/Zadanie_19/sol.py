639838e3612efc38841a2cdf1f3c696a76909b08
Dominik Adamczyk <dominik.adamczyk1000@gmail.com>
2022-08-06 13:15:43 +0200

    added second sollution

--- WDI/Zestaw_2/Zadanie_19/sol.py
+++ WDI/Zestaw_2/Zadanie_19/sol.py
@@ -28,4 +28,45 @@ def f(a, b):
             print(")", end="")
             break
 
-f(1,7)
+# f(1,7)
+
+
+def g(a, b):
+    print(a // b, end="")
+    print(".", end="")
+    flag = True
+    periodStartIdx = float("inf")
+    periodEndIdx = float("inf")
+    aCopy = a
+    a %= b
+    a *= 10
+    idx = 0
+    while flag:
+        # print(a // b, end="")
+        idx += 1
+        aSecond = aCopy
+        a %= b
+        a *= 10
+        for i in range(idx):
+            aSecond %= b
+            aSecond *= 10
+            if a == aSecond:
+                periodStartIdx = i
+                periodEndIdx = idx
+                flag = False
+                break
+
+    a = aCopy
+    a %= b
+    a *= 10
+    for i in range(periodEndIdx):
+        if i == periodStartIdx:
+            print("(", end="")
+        print(a // b, end="")
+        a %= b
+        a *= 10
+
+    print(")", end="")
+
+
+g(1, 3000)
