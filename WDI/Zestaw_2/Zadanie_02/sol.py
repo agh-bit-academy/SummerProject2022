e5e0f93c3681f1bd3785abec1e55c36dcdb154fa
socz3qqq <100852273+socz3qqq@users.noreply.github.com>
2022-08-06 00:26:23 +0000

    D s2t02 (#105)
    * add sol.py, tests
    
    * prog.py, test_big_random change
    
    Co-authored-by: Szczepan Rzeszutek <szcz113@gmail.com>

---
+++ WDI/Zestaw_2/Zadanie_02/sol.py
@@ -0,0 +1,14 @@
+# Szczepan Rzeszutek
+
+# a = int(input())
+# b = int(input())
+# n = int(input())
+
+def f(a, b, n):
+    print(a // b, "." if a % b != 0 else "", sep="", end="")
+    a %= b
+    while a > 0 and n > 0:
+        a *= 10
+        print(a // b, end="")
+        a %= b
+        n -= 1
