03ec1695dae52bac05de47b6b3d28cf074ff03d6
KubaBiz <1kubi1@wp.pl>
2022-08-07 10:44:27 +0200

    prog.py, __init__.py s1t07

--- WDI/Zestaw_1/Zadanie_17/test_A.py
+++
@@ -1,8 +0,0 @@
-# Bartłomiej Kozera
-from ....readstdout import checkstdout
-from .prog import f as user_sol
-from .sol import f as corr_sol
-
-
-def testA():
-    assert checkstdout(user_sol, corr_sol, (), float_type=True)
