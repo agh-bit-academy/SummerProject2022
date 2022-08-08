9517aadf748d4db6012843f9a1812beaaa52072c
Szymon Rusiecki <79122265+Rusiek@users.noreply.github.com>
2022-08-08 18:36:30 +0200

    Update test_medium.py

--- WDI/Zestaw_2/Zadanie_11/test_medium.py
+++ WDI/Zestaw_2/Zadanie_11/test_medium.py
@@ -5,7 +5,6 @@ from .prog import f as user_sol
 from .sol import f as corr_sol
 from random import randint
 
-
 MIN_RANGEE = 10**3
 MAX_RANGE = 10**6
 AMOUNT = 10
@@ -13,7 +12,6 @@ TESTS = [randint(MIN_RANGEE, MAX_RANGE) for i in range(AMOUNT)]
 
 
 @pytest.mark.order(2)
-class TestMedium:
-    @pytest.mark.parametrize("data", TESTS)
-    def test_basic(self, data):
-        assert checkstdout(user_sol, corr_sol, [data])
+@pytest.mark.parametrize("data", TESTS)
+def test_basic(data):
+    assert checkstdout(user_sol, corr_sol, [data])
