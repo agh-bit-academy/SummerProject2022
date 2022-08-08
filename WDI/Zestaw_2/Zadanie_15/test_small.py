23b908f7680feb2a35d8f315ae3c1c3f0510e2c6
socz3qqq <soczawa@student.agh.edu.pl>
2022-08-08 04:32:06 +0200

    s2t15 refactor and Done

---
+++ WDI/Zestaw_2/Zadanie_15/test_small.py
@@ -0,0 +1,14 @@
+# Sebastian Soczawa
+import pytest
+from ....readstdout import checkstdout
+from .prog import f as user_sol
+from .sol import f as corr_sol
+
+BASIC_TESTS = [[1], [2], [3], [4]]
+
+
+@pytest.mark.order(1)
+@pytest.mark.dependency(name="test_basic_s2t15", scope="session")
+@pytest.mark.parametrize("data", BASIC_TESTS)
+def test_basic(data):
+    assert checkstdout(user_sol, corr_sol, data)
