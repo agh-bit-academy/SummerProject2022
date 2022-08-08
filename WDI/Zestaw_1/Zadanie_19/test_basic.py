e612fe2081f9e86d72c12edbecb11e83a4383067
irosikoni <irosikoni@gmail.com>
2022-08-08 12:30:02 +0200

    zadanie19 fixed

--- WDI/Zestaw_1/Zadanie_19/test_basic.py
+++ WDI/Zestaw_1/Zadanie_19/test_basic.py
@@ -1,9 +1,8 @@
 # Izabella Rosikoń
+from ....readstdout import checkstdout
 from .prog import e_number as user_sol
 from .sol import e_number as corr_sol
 
-SET_PRECISION = 0.1 ** 6
 
-
-def test_e():
-    assert abs(user_sol() - corr_sol()) < SET_PRECISION
+def test_basic():
+    assert checkstdout(user_sol, corr_sol, ())
