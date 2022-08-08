a320648063841647c5ad4a704e24fa8988aed0b8
Szymon Rusiecki <79122265+Rusiek@users.noreply.github.com>
2022-08-08 18:34:41 +0200

    Update sol.py

--- WDI/Zestaw_2/Zadanie_10/sol.py
+++ WDI/Zestaw_2/Zadanie_10/sol.py
@@ -3,7 +3,7 @@ def f(x):
     a = 2
     while a < x:
         if x % a == 0:
-            b = x // a  # Dziele całkowicie bo ładniej to wyglądało w outpucie
+            b = x // a
             return a, b
         a = 3 * a + 1
     return False
