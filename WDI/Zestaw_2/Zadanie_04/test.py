15bfa038d67cfa5689a5ca726542659f5aa63a57
Mikołaj Maślak <95372995+M0rgho@users.noreply.github.com>
2022-08-08 14:09:42 +0200

    E s2t04 (#149)
    * s2t04 sol.py
    
    * Added prog.py & test.py to s2t04
    
    Co-authored-by: KubaBiz <1kubi1@wp.pl>
    Co-authored-by: Mikołaj M <nicholasdev7@gmail.com>

---
+++ WDI/Zestaw_2/Zadanie_04/test.py
@@ -0,0 +1,18 @@
+# Mikołaj Maślak
+import pytest
+from .prog import f as user_sol
+from .sol import f as corr_sol
+from random import randint
+
+MIN_RANGE = 1
+MAX_RANGE = 10000
+TEST_NUM = 100
+BASIC_RANDOM_TESTS = [
+    randint(MIN_RANGE, MAX_RANGE)
+    for _ in range(TEST_NUM)]
+
+
+class Test:
+    @pytest.mark.parametrize("data", BASIC_RANDOM_TESTS)
+    def test_basic(self, data):
+        assert user_sol(data) == corr_sol(data)
