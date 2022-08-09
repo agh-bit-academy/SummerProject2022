cc0aae4be063fce06aa046edae84bfa10fce1929
Dominik Adamczyk <dominik.adamczyk1000@gmail.com>
2022-08-09 10:56:58 +0200

    added helper and simple tests

---
+++ WDI/Zestaw_3/Zadanie_10/test_basic.py
@@ -0,0 +1,35 @@
+from gen_helper import gen_test_tab, gen_arith_tab
+import pytest
+from .prog import f as user_sol
+from .sol import f as corr_sol
+from random import randint
+
+EDGE_TESTS = [
+    [1],
+    [1, 2],
+    [0, 0, 0, 0],
+    [1, -2, 3, -4, 5, -6],
+    [0, -1, -2, -3, -4, -5],
+    [1, 2, 3, 2, 1, 0, -1]
+]
+
+LPIECESIZE = 5
+RPIECESIZE = 20
+NOPIECES = 4
+LRANGE = -20
+RRANGE = 20
+MINDIFF = -5
+MAXDIFF = 5
+
+BASIC_RANDOM_TESTS = [gen_test_tab(LPIECESIZE, RPIECESIZE, NOPIECES, LRANGE, RRANGE, MINDIFF, MAXDIFF) for _ in range(10)]
+
+@pytest.mark.order(1)
+@pytest.mark.dependency(name="test_basic_s3t10", scope="session")
+class TestEdge:
+    @pytest.mark.parametrize("data", EDGE_TESTS)
+    def test_edge(self, data):
+        assert user_sol(data) == corr_sol(data)
+
+    @pytest.mark.parametrize("data", BASIC_RANDOM_TESTS)
+    def test_basic_random(self, data):
+        assert user_sol(data) == corr_sol(data)
