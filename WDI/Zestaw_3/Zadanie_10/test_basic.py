9b8751b96b8db9de61c0508f38dcb0f66a2154f6
Dominik Adamczyk <dominik.adamczyk1000@gmail.com>
2022-08-09 13:06:40 +0200

    updated test_basic

--- WDI/Zestaw_3/Zadanie_10/test_basic.py
+++ WDI/Zestaw_3/Zadanie_10/test_basic.py
@@ -7,12 +7,12 @@ from .sol import f as corr_sol
 from random import randint
 
 EDGE_TESTS = [
-    [1],
-    [1, 2],
-    [0, 0, 0, 0],
-    [1, -2, 3, -4, 5, -6],
-    [0, -1, -2, -3, -4, -5],
-    [1, 2, 3, 2, 1, 0, -1]
+    ([1], 1),
+    ([1, 2], 2),
+    ([0, 0, 0, 0], 4),
+    ([1, -2, 3, -4, 5, -6], 2),
+    ([0, -1, -2, -3, -4, -5], 6),
+    ([1, 2, 3, 2, 1, 0, -1], 5)
 ]
 
 LPIECESIZE = 5
@@ -33,9 +33,9 @@ EDGE_RANDOM_TESTS = [gen_arith_tab(randint(LPIECESIZE, RPIECESIZE), randint(LRAN
 @pytest.mark.order(1)
 @pytest.mark.dependency(name="test_basic_s3t10", scope="session")
 class TestEdge:
-    @pytest.mark.parametrize("data", EDGE_TESTS)
-    def test_edge(self, data):
-        assert user_sol(data) == corr_sol(data)
+    @pytest.mark.parametrize("data, ans", EDGE_TESTS)
+    def test_edge(self, data, ans):
+        assert user_sol(data) == ans
 
     @pytest.mark.parametrize("data", EDGE_RANDOM_TESTS)
     def test_edge_random(self, data):
