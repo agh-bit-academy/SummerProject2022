6fe879f9f33dc2756721c2566c5a5f65ab164184
krzyswys <krzyswys3@gmail.com>
2022-08-10 23:48:25 +0200

    fixed problems

--- WDI/Zestaw_2/Zadanie_11/test_small.py
+++ WDI/Zestaw_2/Zadanie_11/test_small.py
@@ -1,6 +1,5 @@
 # Krzysztof Wysocki
 import pytest
-from ....readstdout import checkstdout
 from .prog import f as user_sol
 from .sol import f as corr_sol
 from random import randint
@@ -13,5 +12,5 @@ TESTS = [randint(MIN_RANGEE, MAX_RANGE) for i in range(AMOUNT)]
 
 @pytest.mark.order(1)
 @pytest.mark.parametrize("data", TESTS)
-def test_basic(self, data):
-    assert checkstdout(user_sol, corr_sol, [data])
+def test_small(data):
+    assert user_sol(data) == corr_sol(data)
