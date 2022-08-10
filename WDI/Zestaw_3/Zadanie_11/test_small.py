869b97bfa993ccd178f266f9a8f59e742ccd13ff
krzyswys <krzyswys3@gmail.com>
2022-08-11 01:02:57 +0200

    done tests for 11

---
+++ WDI/Zestaw_3/Zadanie_11/test_small.py
@@ -0,0 +1,28 @@
+# Krzysztof Wysocki
+import pytest
+from .prog import f as user_sol
+from .sol import f as corr_sol
+from random import shuffle
+from random import randint
+
+MIN_RANGEE = 10
+MAX_RANGE = 10**2
+AMOUNT = 10
+
+
+TESTS = []
+for i in range(AMOUNT):
+    A, R, LGT = (
+        randint(MIN_RANGEE, MAX_RANGE),
+        randint(MIN_RANGEE, MAX_RANGE),
+        randint(MIN_RANGEE, MAX_RANGE),
+    )
+    t = [A * R ** (n - 1) for n in range(1, LGT)]
+    shuffle(t)
+    TESTS.append(t)
+
+
+@pytest.mark.order(1)
+@pytest.mark.parametrize("data", TESTS)
+def test_small(data):
+    assert user_sol(data) == corr_sol(data)
