2d86d6f0d3329af2422688ef5e33acf62f1a1860
socz3qqq <soczawa@student.agh.edu.pl>
2022-08-11 13:44:51 +0200

    added tests, and modified sol.py

--- WDI/Zestaw_2/Zadanie_10/test_basic.py
+++ WDI/Zestaw_2/Zadanie_10/test_basic.py
@@ -1,26 +1,32 @@
 # Sebastian Soczawa
 import pytest
-from ....readstdout import checkstdout
 from .prog import f as user_sol
 from .sol import f as corr_sol
 from random import randint
 
-BASIC_RANDOM_TESTS = [randint(1, 100) for _ in range(20)]
-
-MIN_RANGE = 10**10
-MAX_RANGE = 10**15
-TEST_NUM = 80
-BIG_RANDOM_TESTS = [
-    randint(MIN_RANGE, MAX_RANGE)
-    for _ in range(TEST_NUM)
+BASIC_MIN_RANGE = 1
+BASIC_MAX_RANGE = 1000
+BASIC_TEST_NUM = 20
+BASIC_RANDOM_TESTS = [
+    randint(BASIC_MIN_RANGE, BASIC_MAX_RANGE)
+    for _ in range(BASIC_TEST_NUM)
 ]
+
+BIG_MIN_RANGE = 10**10
+BIG_MAX_RANGE = 10**15
+BIG_TEST_NUM = 80
+BIG_RANDOM_TESTS = [
+    randint(BIG_MIN_RANGE, BIG_MAX_RANGE)
+    for _ in range(BIG_TEST_NUM)
+]
+
 
 @pytest.mark.dependency(name="test_basic_s2ex7", scope="session")
 class TestBasic:
     @pytest.mark.parametrize("data", BASIC_RANDOM_TESTS)
     def test_basic(self, data):
-        assert checkstdout(user_sol, corr_sol, [data])
+        assert user_sol(data) == corr_sol(data)
 
     @pytest.mark.parametrize("data", BIG_RANDOM_TESTS)
     def test_basic_random(self, data):
-        assert checkstdout(user_sol, corr_sol, [data])
+        assert user_sol(data) == corr_sol(data)
