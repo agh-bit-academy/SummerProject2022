75f8ec16333546ac95b898323cff244f7d153745
Andrzej Karciński <67584332+akarcinski@users.noreply.github.com>
2022-08-12 20:22:28 +0200

    S3t15 (#193)
    * s3t15 solution
    
    * s3t15 update
    
    * s3t15 solution
    
    * s3t15

---
+++ WDI/Zestaw_3/Zadanie_15/sol.py
@@ -0,0 +1,35 @@
+# Andrzej Karciński
+
+def isPrime(x):
+    if x < 2:
+        return False
+    i = 2
+    while i * i <= x:
+        if x % i == 0:
+            return False
+        i += 1
+    return True
+
+
+def f(array):
+    a = 1
+    b = 2
+    c = 3
+    primeNumber = False
+
+    if isPrime(array[0]):
+        return False
+
+    for i in range(len(array)):
+        if i == a:
+            a, b = b, c
+            c = a + b
+            if isPrime(array[i]) is True:
+                return False
+        else:
+            if isPrime(array[i]) is True:
+                primeNumber = True
+
+    if primeNumber:
+        return True
+    return False
