168f547afac36c20c7970c7c4af8ad98df31ad03
VeryLongNicknameSuchWow <d33p4ck@gmail.com>
2022-08-13 17:51:59 +0200

    s3t17 sol fix for arrays of different length

--- WDI/Zestaw_3/Zadanie_17/sol.py
+++ WDI/Zestaw_3/Zadanie_17/sol.py
@@ -1,11 +1,18 @@
 # Maciej Bartczak
 def f(A, B):
-    length = len(A)
+    a_length = len(A)
+    b_length = len(B)
+    common_length = min(a_length, b_length)
+    max_length = max(a_length, b_length)
+    longer = A
+    if b_length > a_length:
+        longer = B
+
     valid_sum_counter = 0
-    for mask in range(0, 3 ** length):
+    for mask in range(0, 3 ** common_length):
         result = 0
         index = 0
-        while index < length:
+        while index < common_length:
             digit = mask % 3
             mask //= 3
             if digit == 0:
@@ -14,6 +21,9 @@ def f(A, B):
                 result += B[index]
             elif digit == 2:
                 result += A[index] + B[index]
+            index += 1
+        while index < max_length:
+            result += longer[index]
             index += 1
         is_prime = True
         for divisor in range(2, result):
