401cec121086ec6521d132ef654c985ff7de4cdb
VeryLongNicknameSuchWow <d33p4ck@gmail.com>
2022-08-14 21:09:49 +0200

    s3t02 sol improvements

--- WDI/Zestaw_3/Zadanie_02/sol.py
+++ WDI/Zestaw_3/Zadanie_02/sol.py
@@ -1,4 +1,4 @@
-# Izabella Rosikoń
+# Izabella Rosikoń, Maciej Bartczak
 import math
 
 
@@ -6,22 +6,18 @@ def f(a, b):
     lengthA = int(math.log10(a)) + 1
     lengthB = int(math.log10(b)) + 1
 
-    listA = [0] * lengthA
-    listB = [0] * lengthB
-
     if lengthA != lengthB:
         return False
-    else:
-        for j in range(lengthB):
-            listA[j] = a % 10
-            listB[j] = b % 10
-            a = a // 10
-            b = b // 10
-    tab = [0] * 10
 
-    for item in listA:
-        tab[item] = tab[item] + 1
-    for jtem in listB:
-        tab[jtem] = tab[jtem] - 1
+    digit_counter = [0 for _ in range(10)]
 
-    return tab == [0] * 10
+    for _ in range(lengthA):
+        digit_counter[a % 10] += 1
+        a //= 10
+        digit_counter[b % 10] -= 1
+        b //= 10
+
+    for count in digit_counter:
+        if count != 0:
+            return False
+    return True
