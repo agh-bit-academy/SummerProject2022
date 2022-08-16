04ff3ddca10e1e137796880b6b63e3ba81a5f97f
NiEkOKsU <bartek.kozera00@gmail.com>
2022-08-17 00:11:28 +0200

    sol changed

--- WDI/Zestaw_4/Zadanie_01/sol.py
+++ WDI/Zestaw_4/Zadanie_01/sol.py
@@ -5,20 +5,28 @@ def f(A):
     max_num = n * m
     x, y = 0, 0
     while curr_num <= max_num:
-        while x < m - 1 and A[y][x + 1] == 0:
+        while x < m and A[y][x] == 0:
             A[y][x] = curr_num
             x += 1
             curr_num += 1
-        while y < n - 1 and A[y + 1][x] == 0:
+        x -= 1
+        y += 1
+        while y < n and A[y][x] == 0:
             A[y][x] = curr_num
             y += 1
             curr_num += 1
-        while x > 0 and A[y][x - 1] == 0:
+        y -= 1
+        x -= 1
+        while x >= 0 and A[y][x] == 0:
             A[y][x] = curr_num
             x -= 1
             curr_num += 1
-        while y > 0 and A[y - 1][x] == 0:
+        x += 1
+        y -= 1
+        while y >= 0 and A[y][x] == 0:
             A[y][x] = curr_num
             y -= 1
             curr_num += 1
+        y += 1
+        x += 1
     return A
