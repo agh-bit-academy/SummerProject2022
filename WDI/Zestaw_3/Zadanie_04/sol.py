82e7dc3005919c12286353d860b4332e1a02aebc
kkkucia <11.karolinakucia@gmail.com>
2022-08-16 15:40:46 +0200

    B_s3t04_sol

--- WDI/Zestaw_3/Zadanie_04/sol.py
+++ WDI/Zestaw_3/Zadanie_04/sol.py
@@ -1,3 +1,22 @@
+# Karolina Kucia
 def f(n):
-    # tutaj wpisz swoją implementację
-    pass
+    add = [0 for _ in range(n + 1)]
+    add[0] = 1
+    mul = [0 for _ in range(n + 1)]
+    mul[0] = 1
+    i = 1
+    while max(mul) > 0:
+        rest = 0
+        tmp = 0
+        for j in range(n, -1, -1):
+            tmp = add[j] + mul[j] + rest
+            rest = tmp // 10
+            add[j] = tmp % 10
+        i += 1
+        r = 0
+        for k in range(n + 1):
+            r *= 10
+            r += mul[k]
+            mul[k] = r // i
+            r %= i
+    return add
