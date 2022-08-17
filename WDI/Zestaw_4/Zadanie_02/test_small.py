ab8d683697c0de8a0156179e114c572741aa5f2b
Szymon Rusiecki <79122265+Rusiek@users.noreply.github.com>
2022-08-17 15:33:15 +0200

    Update test_small.py

--- WDI/Zestaw_4/Zadanie_02/test_small.py
+++ WDI/Zestaw_4/Zadanie_02/test_small.py
@@ -13,7 +13,7 @@ TEST_NUM = 20
 SMALL_RANDOM_TESTS = [[RandFixArray(SIZE, LRANGE, RRANGE).get() for _ in range(SIZE)] for _ in range(TEST_NUM)]
 
 
-@pytest.mark.order(3)
+@pytest.mark.order(2)
 @pytest.mark.dependency(name="test_small_s4t02", depends=["test_basic_s4t02"], scope="session")
 @pytest.mark.parametrize("data", SMALL_RANDOM_TESTS)
 def test_small_random(data):
