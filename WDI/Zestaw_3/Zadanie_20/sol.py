ec36e63efe1cda131900648de83cd5ccf51ea1e7
ksew1 <karol.sewilo@gmail.com>
2022-08-17 16:37:08 +0200

    Sol

---
+++ WDI/Zestaw_3/Zadanie_20/sol.py
@@ -0,0 +1,40 @@
+def f(t):
+    n = len(t)
+    array_of_divisors = [divisors(t[i]) for i in range(n)]
+
+    result = 0
+    for i in range(n):
+        result = max(count_longest_subsequence(i, array_of_divisors, n), result)
+    return result
+
+
+def count_longest_subsequence(i, array_of_divisors, n):
+    array = [False for _ in range(1000)]
+    counter = 0
+    for j in range(i, n):
+        if len(array_of_divisors[j]) == 0:
+            return counter
+        for nr in array_of_divisors[j]:
+            if array[nr] is True:
+                return counter
+            array[nr] = True
+        counter += 1
+    return counter
+
+
+def divisors(number):
+    i = 2
+    array = []
+    while number > 1 and i ** 2 <= number:
+        if number % i == 0:
+            if len(array) != 0 and i == array[-1]:
+                return []
+            array.append(i)
+            number //= i
+        else:
+            i += 1
+    if number > 1:
+        if len(array) != 0 and array[-1] == number:
+            return []
+        array.append(number)
+    return array
