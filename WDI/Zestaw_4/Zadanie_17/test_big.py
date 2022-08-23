77b1b84c7a486c20baef04c4f4caed83da4a16c4
kkkucia <11.karolinakucia@gmail.com>
2022-08-23 10:46:33 +0200

    s4t17_test

---
+++ WDI/Zestaw_4/Zadanie_17/test_big.py
@@ -0,0 +1,20 @@
+# Karolina Kucia
+import pytest
+from ....Rand_Templates.RandFixArray import RandFixArray
+from .prog import f as user_sol
+from .sol import f as corr_sol
+
+
+SIZE = 100
+LRANGE = 1
+RRANGE = 10 ** 6
+TEST_NUM = 25
+
+BIG_RANDOM_TESTS = [[RandFixArray(SIZE, LRANGE, RRANGE).get() for _ in range(SIZE)] for _ in range(TEST_NUM)]
+
+
+@pytest.mark.order(3)
+@pytest.mark.dependency(name="test_big_s4t17", depends=["test_small_s4t17"], scope="session")
+@pytest.mark.parametrize("data", BIG_RANDOM_TESTS)
+def test_big_random(data):
+    assert user_sol(data) == corr_sol(data)
