9ee0b4da32f40ff3220e0d32baf798f50228f796
Szymon Rusiecki <79122265+Rusiek@users.noreply.github.com>
2022-09-19 07:56:09 +0200

    [rfll_gen] Rework generator and overload __eq__

--- Rand_Templates/RandFixLinkedList.py
+++ Rand_Templates/RandFixLinkedList.py
@@ -3,19 +3,44 @@ from random import randint
 class Node():
     rfll_seed = 0
 
-    def __init__(self, val):
+    def __init__(self, val=None):
         self.key = val
         self.next = None
 
     @staticmethod
-    def gen_random(
+    def gen_two_random(
         size: int, l_range: int, r_range: int,
         rand_gen=randint
     ):
-        head = Node(None)
-        tail = head
+        head_A = Node(None)
+        head_B = Node(None)
+        tail_A = head_A
+        tail_B = head_B
         for _ in range(size):
-            tail.next = Node(rand_gen(l_range, r_range))
-            tail = tail.next
+            val = rand_gen(l_range, r_range)
+            tail_A.next = Node(val)
+            tail_B.next = Node(val)
+            tail_A = tail_A.next
+            tail_B = tail_B.next
 
-        return head
+        return head_A, head_B
+
+    def __eq__(A, B):
+        if not type(A) == Node or not type(B) == Node:
+            return False
+
+        if A.key == None:
+            A = A.next
+        if B.key == None:
+            B = B.next
+
+        while A and B:
+            if A.key != B.key:
+                return False
+            A = A.next
+            B = B.next
+
+        if A or B:
+            return False
+
+        return True
