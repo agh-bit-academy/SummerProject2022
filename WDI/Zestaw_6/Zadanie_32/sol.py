7d1cdf9aceed430878ac808be274fa9785af7400
irosikoni <irosikoni@gmail.com>
2022-09-20 12:59:36 +0200

    changed len(T)-1 to len(T)

--- WDI/Zestaw_6/Zadanie_32/sol.py
+++ WDI/Zestaw_6/Zadanie_32/sol.py
@@ -3,7 +3,7 @@ def f(T, k):
     def rec(T, k, sum_1, sum_2, elements_1, elements_2, i):
         if sum_1 == sum_2 and elements_1 + elements_2 == k:
             return True
-        if i == len(T) - 1:
+        if i == len(T):
             return False
         a = rec(T, k, sum_1 + T[i], sum_2, elements_1 + 1, elements_2, i + 1)
         b = rec(T, k, sum_1, sum_2 + T[i], elements_1, elements_2 + 1, i + 1)
